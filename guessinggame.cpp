fb8445f8d58edc20ceca976f4be4fb5dcda1cca7
U-BSD\323978 <323978@SHS-10DD3AC1E.beaverton.k12.or.us>
2017-09-21 11:08:45 -0700

    first commit of C++!

---
+++ guessinggame.cpp
@@ -0,0 +1,44 @@
+# include <iostream>
+# include <cstdlib>
+# include <ctime>
+
+using namespace std;
+
+int main() {
+
+  int input = 0;
+  int counter = 0;
+
+  srand(time(NULL));
+  
+  int randNum = rand() % 101;
+
+  cout << randNum << endl;
+  
+  cout << "A random number was generated. Time to guess! ";
+
+  while (true) {
+    cin >> input;
+    counter ++;
+    if (input > randNum) {
+      cout << "The number you guessed is higher than the random number. Guess again: ";
+    } else if (input < randNum) {
+      cout << "The number you guessed is lower than the random number. Guess again: ";
+    } else {
+      cout << "The number you guessed is the random number! ";
+      if (counter == 1) {
+	cout << "It took you " << counter << " guess." << endl;
+      } else {
+	cout << "It took you " << counter << " guesses." << endl;
+      }
+      cout << "Do you want to play again? Type y/n: ";
+      cin >> input;
+      if (input == 'y') {
+	randNum = rand() % 101;
+	cout << "A random number was generated. Time to guess! ";
+      } else {
+	return false;
+      }
+    } 
+  }
+}
