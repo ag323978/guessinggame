63af2219c0dd20b0f94cc9585183383128ab217d
U-BSD\323978 <323978@SHS-10DD3AC1E.beaverton.k12.or.us>
2017-09-21 11:26:25 -0700

    Finished version of guessing game

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -6,36 +6,47 @@ using namespace std;
 
 int main() {
 
+  //input variable
   int input = 0;
+  //keeps track of number of guesses
   int counter = 0;
 
+  //random number generator seed
   srand(time(NULL));
-  
+
+  //generate random number between 0-100 inclusive
   int randNum = rand() % 101;
   
   cout << "A random number was generated. Time to guess! ";
 
+  //keep guessing until guess is correct
   while (true) {
     cin >> input;
     counter ++;
+    //if guess is too high 
     if (input > randNum) {
       cout << "The number you guessed is higher than the random number. Guess again: ";
-    } else if (input < randNum) {
+    } // if guess is too low
+    else if (input < randNum) {
       cout << "The number you guessed is lower than the random number. Guess again: ";
-    } else {
+    } //otherwise you guessed the random number correctly
+    else {
       cout << "The number you guessed is the random number! ";
       if (counter == 1) {
 	cout << "It took you " << counter << " guess." << endl;
       } else {
 	cout << "It took you " << counter << " guesses." << endl;
       }
+      //ask user if they want to play again
       cout << "Do you want to play again? Type y/n: ";
       char character;
       cin >> character;
+      //generate another random number if they answer yes
       if (character == 'y') {
 	randNum = rand() % 101;
 	cout << "A random number was generated. Time to guess! ";
-      } else {
+      } //otherwise break out of loop
+      else {
 	break;
       }
     } 
